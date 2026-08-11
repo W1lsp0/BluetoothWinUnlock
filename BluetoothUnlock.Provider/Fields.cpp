fbf5f8c02a0ef6029635c23ade91b45838e1a913
W1lsp0 <w1lsp0@foxmail.com>
2026-08-11 17:22:15 +0800

    Fix Windows CI restore and field GUIDs

--- BluetoothUnlock.Provider/Fields.cpp
+++ BluetoothUnlock.Provider/Fields.cpp
@@ -2,10 +2,10 @@
 
 const CREDENTIAL_PROVIDER_FIELD_DESCRIPTOR g_fieldDescriptors[FID_NUM_FIELDS] =
 {
-    { FID_TILE_IMAGE, CPFT_TILE_IMAGE, const_cast<PWSTR>(L"Bluetooth Unlock"), CPFG_CREDENTIAL_PROVIDER_LOGO },
+    { FID_TILE_IMAGE, CPFT_TILE_IMAGE, const_cast<PWSTR>(L"Bluetooth Unlock"), GUID_NULL },
     { FID_LARGE_TEXT, CPFT_LARGE_TEXT, const_cast<PWSTR>(L"Bluetooth Unlock"), GUID_NULL },
     { FID_STATUS_TEXT, CPFT_SMALL_TEXT, const_cast<PWSTR>(L"Waiting for verified Bluetooth device"), GUID_NULL },
-    { FID_SUBMIT_BUTTON, CPFT_SUBMIT_BUTTON, const_cast<PWSTR>(L"Unlock"), CPFG_CREDENTIAL_PROVIDER_SUBMIT_BUTTON }
+    { FID_SUBMIT_BUTTON, CPFT_SUBMIT_BUTTON, const_cast<PWSTR>(L"Unlock"), GUID_NULL }
 };
 
 const FIELD_STATE_PAIR g_fieldStatePairs[FID_NUM_FIELDS] =
